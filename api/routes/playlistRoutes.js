8ee92d5d190d7969a8d1bf51f0d35098e5ec82c0
rauldosS <48498755+rauldosS@users.noreply.github.com>
2019-10-27 13:57:42 -0300

    Update

--- api/routes/playlistRoutes.js
+++ api/routes/playlistRoutes.js
@@ -3,11 +3,12 @@
 module.exports = function (app) {
     var playlists = require('../controller/playlistController');
     // messages Routes
+    app.route('/playlists/post')
+        .post(playlists.create_a_playlist)
     app.route('/playlists')
         .get(playlists.list_all_playlists)
-        .post(playlists.create_a_playlist);
     app.route('/playlists/:playlistId')
         .get(playlists.read_a_playlist)
         .put(playlists.update_a_playlist)
-        .delete(playlists.delete_a_playlist);
+        .delete(playlists.delete_a_playlist)
 }
--- front-end/js/createElements.js
+++ front-end/js/createElements.js
@@ -7,33 +7,32 @@ function createElements() {
 
     myObj.data.forEach(video => {
         nrVideo++
-        
+
         console.log(nrVideo)
 
-        let p = document.createElement('p')
         let a = document.createElement('a')
 
         let divVideos = document.getElementById('videos')
-        
+
         // Card
         let divCard = document.createElement('div')
         divCard.setAttribute('id', 'card' + nrVideo)
         divCard.setAttribute('class', 'card white')
-    
+
         divVideos.appendChild(divCard)
 
         let divCardDoc = document.getElementById('card' + nrVideo)
-        
+
         //span.textContent = "qualquer coisa"
         //divCardDoc.appendChild(span)
-        
-        
+
+
         // Card image/span
         let divCardImage = document.createElement('div')
 
         divCardImage.setAttribute('id', 'card-image' + nrVideo)
         divCardImage.setAttribute('class', 'card-image')
-        
+
         divCardDoc.appendChild(divCardImage)
 
         let divCardImageDoc = document.getElementById('card-image' + nrVideo)
@@ -42,12 +41,77 @@ function createElements() {
         let spanCardTitle = document.createElement('span')
 
         spanCardTitle.setAttribute('class', 'card-title')
-        
-        spanCardTitle.textContent('video')
+        spanCardTitle.setAttribute('id', 'title' + nrVideo)
 
         divCardImageDoc.appendChild(spanCardTitle)
+
+        let title = document.querySelector('#title' + nrVideo)
+        title.textContent = video.title
+
+        // Card content
+        let divCardContent = document.createElement('div')
+
+        divCardContent.setAttribute('id', 'card-content' + nrVideo)
+        divCardContent.setAttribute('class', 'card-content')
+
+        divCardDoc.appendChild(divCardContent)
+
+        let divCardContentDoc = document.getElementById('card-content' + nrVideo)
+
+        // Card-title
+        let pCardSlug = document.createElement('p')
+
+        pCardSlug.setAttribute('class', 'slug')
+        pCardSlug.setAttribute('id', 'slug' + nrVideo)
+
+        divCardContentDoc.appendChild(pCardSlug)
+
+        let slug = document.querySelector('#slug' + nrVideo)
+        slug.textContent = video.slug
+
+        // Card action
+        let divCardAction = document.createElement('div')
+
+        divCardAction.setAttribute('id', 'card-action' + nrVideo)
+        divCardAction.setAttribute('class', 'card-action')
+
+        divCardDoc.appendChild(divCardAction)
+
+        let divCardActionDoc = document.getElementById('card-action' + nrVideo)
+
+        // Criar condição para adiconar ou remover
+        // Card-add
+        let aCardAdd = document.createElement('a')
+
+        aCardAdd.setAttribute('class', 'btn')
+        aCardAdd.setAttribute('id', 'add' + nrVideo)
+        // aCardAdd.setAttribute('href', 'http://localhost:3000/playlists')
+        aCardAdd.setAttribute('onclick', 'addPlaylist(this.id)')
+
+        divCardActionDoc.appendChild(aCardAdd)
+
+        let add = document.querySelector('#add' + nrVideo)
+        add.textContent = 'ADICIONAR'
 
         // a.setAttribute('href', url)
         // a.setAttribute('href', adicionar/remover)        
     });
+}
+
+function addPlaylist(id) {
+    let idDivVideo = document.querySelector('#' + id).parentElement.parentElement.id
+
+    id = idDivVideo.substr(-1)
+
+    let myJSON = '{ "current_page": 1, "data": [{ "id": 42, "title": "Super Miranha (2020)", "description": "O melhor filme do homem aranha j\u00e1 feito", "slug": "homenaranha", "created_at": null, "updated_at": null, "deleted_at": null, "category_id": 2, "url": "", "thumbnail": "" }, { "id": 52, "title": "Harry Potter e o bagulho dourado", "description": "Um moleque meio bruxo", "slug": "harry", "created_at": null, "updated_at": null, "deleted_at": null, "category_id": 2, "url": "", "thumbnail": "" }, { "id": 252, "title": "Homem-Aranha 2 (2004)", "description": "O melhor filme do homem aranha j\u00e1 feito", "slug": "spiderman", "created_at": null, "updated_at": null, "deleted_at": null, "category_id": 2, "url": "", "thumbnail": "" }, { "id": 292, "title": "Miranha O Filme", "description": "O melhor filme do homem aranha j\u00e1 feito", "slug": "spermiranha", "created_at": null, "updated_at": null, "deleted_at": null, "category_id": 2, "url": "", "thumbnail": "" }, { "id": 1722, "title": "Santiago Biali", "description": "Too easy", "slug": "santiago-biali", "created_at": null, "updated_at": null, "deleted_at": null, "category_id": 2, "url": "", "thumbnail": "" }, { "id": 1762, "title": "Estevan Will", "description": "ioaofiaifa", "slug": "Estevan Will", "created_at": null, "updated_at": null, "deleted_at": null, "category_id": 2, "url": "", "thumbnail": "" }, { "id": 1832, "title": "Manoel Souza", "description": "ASDASDASDASDASD", "slug": "manoel-souza-s", "created_at": null, "updated_at": null, "deleted_at": null, "category_id": 2, "url": "", "thumbnail": "" }, { "id": 1852, "title": "Estevan Will", "description": "ioaofiaifa", "slug": "aaa", "created_at": null, "updated_at": null, "deleted_at": null, "category_id": 2, "url": "", "thumbnail": "" }, { "id": 1862, "title": "Jo\u00e3o Gabriel Zago Alves", "description": "A hist\u00f3ria de um aluno com problemas", "slug": "joao-zago-alves", "created_at": null, "updated_at": null, "deleted_at": null, "category_id": 2, "url": "", "thumbnail": "" }, { "id": 1922, "title": "Jo\u00e3o Gabriel Zago Alves", "description": "A hist\u00f3ria de um aluno com problemas", "slug": "joao-gabriel-zago-alves", "created_at": null, "updated_at": null, "deleted_at": null, "category_id": 2, "url": "", "thumbnail": "" }], "first_page_url": "http:\/\/unoflix-video.herokuapp.com\/videos?page=1", "from": 1, "last_page": 3, "last_page_url": "http:\/\/unoflix-video.herokuapp.com\/videos?page=3", "next_page_url": "http:\/\/unoflix-video.herokuapp.com\/videos?page=2", "path": "http:\/\/unoflix-video.herokuapp.com\/videos", "per_page": 10, "prev_page_url": null, "to": 10, "total": 25 }'
+    let myObj = JSON.parse(myJSON)
+    
+    console.log(myObj.data[id].title)
+
+    add = {
+        "user": myObj.data[id],
+        "slug": "teste-raul02",
+        "watched": true
+    }
+
 }
